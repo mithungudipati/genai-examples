177aa963d334fc9c2dedf763003c9f29fda54c19
mithungudipati <46465379+mithungudipati@users.noreply.github.com>
2025-04-16 12:49:04 -0700

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -31,7 +31,7 @@ def scrape_yaml_examples(url):
         yaml_examples = soup.find_all("pre", {"class": "language-yaml"})
         
         if yaml_examples:
-            print(f"Found {len(yaml_examples)} YAML examples on {url}")
+            print(f"Found and {len(yaml_examples)} YAML examples on {url}")
             
             # Extract YAML examples and save them to files
             for idx, example in enumerate(yaml_examples):
